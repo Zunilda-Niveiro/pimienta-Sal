3bc11ae1d768c1e45f2675428baabfee5a58876a
Zunilda-Niveiro <niveiro.zunilda@gmail.com>
2022-08-17 21:43:06 -0300

    tp pimientay sal

---
+++ src/data/products.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id : 1,
+        Title : "carpaccio fresco",
+        descripcion : "Entrada Carpaccio de salmón con cítricos",
+        price :  65.50, 
+        image : "Carpaccio-de-salmon.jpg"
+    },
+    {
+      id : 2,
+      Title : "Risotto de berenjena",
+      descripcion : " Risotto de berenjena y queso de cabra",
+      price : 47.00, 
+      image : "Risotto-berenjena-queso-cabra.jpg"
+    },
+    {
+    id : 3,
+    Title : "Mousse de arroz",
+    descripcion : " Mousse de arroz con leche y aroma de azahar",
+    price : 27.50,  
+    image : "Mousse-de-arroz-con-leche.jpg"
+    },
+    {
+    id : 4,
+    Title : "Espárragos blancos",
+    descripcion : "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+    price : 37.50,  
+    image : "esparragos.png"
+    },
+]
+
